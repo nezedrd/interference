962f5837c29e0b20b7699e01c5d7c8d9e28fa7bf
neze <clement@neze.fr>
2018-04-20 11:53:29 +0900

    basic image display

--- demo/__main__.py
+++ demo/__main__.py
@@ -1,1 +1,9 @@
-print(__name__)
+import numpy as np
+from matplotlib import pyplot as plt
+
+if __name__=='__main__':
+    data = np.zeros((512,512),dtype=np.float)
+    for i in reversed(range(1,101)):
+        data[256-2*i:256+2*i,256-2*i:256+2*i] = (100.-i)/100.
+    plt.imshow(data, interpolation='nearest', cmap=plt.get_cmap('Greys'))
+    plt.show()
