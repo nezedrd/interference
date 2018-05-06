87a09f466b2076327a36748ddfa5d3df7c2bdb7f
neze <clement@neze.fr>
2018-05-06 16:31:30 +0900

    some update

--- young/__main__.py
+++ young/__main__.py
@@ -1,23 +1,31 @@
 from logging import basicConfig,getLogger,DEBUG,INFO,WARNING,ERROR,CRITICAL
 basicConfig(level=DEBUG)
 
-import os,argparse
+from os.path import sep
+from argparse import ArgumentParser,REMAINDER
 from .tools import log_test
 from importlib import import_module
-logger = getLogger(__name__)
+
+if __name__=='__main__':
+    prog_name = __file__.split(sep)[-2]
+else:
+    prog_name = __name__
+logger = getLogger(prog_name)
+
 
 def parse_args():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('module')
+    ap = ArgumentParser(prog=prog_name)
+    ap.add_argument('module',metavar='demo_name',
+            help="Name of the demo in the demos/ folder.")
+    ap.add_argument('remainder',nargs=REMAINDER,default=list(),
+            help="Everything that should be passed to the demo.")
     return ap.parse_args()
 
 def main():
     argv = parse_args()
-    # m = import_module('.'+argv.module,package=__file__.split(os.path.sep)[-2])
-    m = import_module('.demos.'+argv.module,package=__file__.split(os.path.sep)[-2])
+    m = import_module('.demos.'+argv.module,package=prog_name)
     logger.info("Finished import. Starting.")
-    m.run()
+    m.run(argv.remainder)
 
 if __name__=='__main__':
-    logger = getLogger(__file__.split(os.path.sep)[-2])
     main()
--- young/demos/pyplot.py
+++ young/demos/pyplot.py
@@ -1,7 +1,15 @@
 from matplotlib.pyplot import show
+from argparse import ArgumentParser
+from logging import getLogger
 from ..demo import YoungDemo
+logger = getLogger(__name__)
 
-def run():
+def parse_args(argv):
+    ap = ArgumentParser(prog=__name__)
+    return ap.parse_args(argv)
+
+def run(argv):
+    args = parse_args(argv)
     y = YoungDemo()
     y.figure()
     print(y)
