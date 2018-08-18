cac1a48f30bfe64f4ae23d218bb1ebde0c94bac3
neze <clement@neze.fr>
2018-08-18 10:47:26 +0900

    [pypi] package
    Signed-off-by: neze <clement@neze.fr>

--- young/demos/movie.py
+++ young/demos/movie.py
@@ -10,6 +10,9 @@ logger = getLogger(__name__)
 
 def parse_args(argv):
     ap = ArgumentParser(prog=__name__)
+    ap.add_argument('-i',metavar='filename',dest='input',
+            default=None,
+            help="Input yaml or json file.")
     ap.add_argument('-o',metavar='filename',dest='output',
             default='output.mp4',
             help="Output file.")
@@ -23,11 +26,7 @@ def parse_args(argv):
 
 def run(argv):
     args = parse_args(argv)
-
-    # 1920*1080: (19.2,10.8), alpha=1
-    # 1280* 720: (12.8,7.20), alpha=2/3
-    #  960* 540: (9.60,5.40), alpha=1/2
-    params = {
+    get_quality = {
             'hhd': {
                 'figsize': (19.2,10.8),
                 'dpi': 120.00,
@@ -54,117 +53,150 @@ def run(argv):
                 'alpha': 3,
                 },
             }
-    fps=15
-    # figsize=params[args.res]['figsize']
+    # SET DEFAULT CONFIGURATION INCLUDING STEPS
     figsize=(16,9)
-    dpi=params[args.res]['dpi']
-    alpha=params[args.res]['alpha']
+    fps=15
+    resolution = args.res
 
-    def seconds(s):
-        return int(s*fps)
+    movie_title = "Interference Video"
+    movie_artist = "nezedrd"
+    movie_comment = "void"
+
+    steps = [
+        {
+        "var": "wl",
+        "sec": 10,
+        "end": 532
+        },
+        {
+        "var": "d",
+        "sec": 20,
+        "end": 5
+        },
+        {
+        "var": "y",
+        "end": 3.5
+        },
+        {
+        "sec": 5,
+        "end": 20
+        },
+        {
+        "var": "p",
+        "sec": 20,
+        "end": 620
+        },
+        {
+        "var": "wl",
+        "end": 680
+        },
+        {
+        "sec": 40,
+        "end": 400
+        }
+    ]
+
+    # GET EVERY CONFIGURATION VALUE
+    if args.input is not None:
+        pass
+
+    # CREATE CONFIGURATIONS AND SET INITIAL VALUES
+    quality = get_quality[resolution]
+    dpi = quality['dpi']
+    alpha = quality['alpha']
 
     dc = DisplayConfig()
     dc.res /= alpha
-
     yc = YoungConfig()
-    yc.wl = 400
 
+    # CREATE DEMO
     y = YoungDemo(display_cfg=dc,young_cfg=yc)
     print(y)
 
+    # CONFIGURE AND CREATE FIGURE
     y.figure_config(figsize=figsize,dpi=dpi)
-
     fig = y.figure()
 
+    # CREATE GETTERS AND SETTERS
+    def seconds(s):
+        return int(s*fps)
     def slow_range(start,stop,num):
         r = cos(linspace(0,pi,num=num))
         return (stop-start)*((1-r)/2)+start
-
     def d_getter():
         return y.d_slider.val
     def d_setter(x):
         y.d_slider.set_val(x)
-
     def y_getter():
         return y.y_slider.val
     def y_setter(x):
         y.y_slider.set_val(x)
-
     def p_getter():
         return y.p_slider.val
     def p_setter(x):
         y.p_slider.set_val(x)
-
     def wl_getter():
         return y.wl_slider.val
     def wl_setter(x):
         y.wl_slider.set_val(x)
+    getters = {
+            'd': d_getter,
+            'y': y_getter,
+            'p': p_getter,
+            'wl': wl_getter,
+            }
+    setters = {
+            'd': d_setter,
+            'y': y_setter,
+            'p': p_setter,
+            'wl': wl_setter,
+            }
 
-    steps = list()
-    steps.append({
-        'nframes': seconds(10.),
-        'getter': wl_getter,
-        'to': 532,
-        'setter': wl_setter,
-        })
-    steps.append({
-        'nframes': seconds(20.),
-        'getter': d_getter,
-        'to': 5,
-        'setter': d_setter,
-        })
-    steps.append({
-        'nframes': seconds(20.),
-        'getter': y_getter,
-        'to': 3.5,
-        'setter': y_setter,
-        })
-    steps.append({
-        'nframes': seconds(5.),
-        'getter': y_getter,
-        'to': 20,
-        'setter': y_setter,
-        })
-    steps.append({
-        'nframes': seconds(20.),
-        'getter': p_getter,
-        'to': 620,
-        'setter': p_setter,
-        })
-    steps.append({
-        'nframes': seconds(20.),
-        'getter': wl_getter,
-        'to': 680,
-        'setter': wl_setter,
-        })
-    steps.append({
-        'nframes': seconds(40.),
-        'getter': wl_getter,
-        'to': y.wl_slider.valmin,
-        'setter': wl_setter,
-        })
-
-    nframes = sum([ s['nframes'] for s in steps ])
-
+    # CREATE FFMPEG WRITER
     FFMpegWriter = writers['ffmpeg']
     metadata = {
-            'title': 'Young Interference - Animation',
-            'artist': 'Clement',
+            'title': movie_title,
+            'artist': movie_artist,
+            'comment': movie_comment,
         }
     writer = FFMpegWriter(fps=fps, metadata=metadata)
 
-    fig.savefig(args.output+'.png')
-
+    # START WRITING
     if args.display:
         ion()
         show()
+
+    s0 = steps[0]
+    if 'var' not in s0 or 'sec' not in s0 or 'end' not in s0:
+        logger.critical("First step should be completely set up.")
+        raise KeyError
+    s0['nframes'] = seconds(s0['sec'])
+    for i in range(1,len(steps)):
+        t = steps[i-1]
+        s = steps[i]
+        s['var'] = s.get('var',t['var'])
+        if s['var'] not in getters:
+            logger.critical("Variable should be among {:}.".format(getters.keys()))
+            raise KeyError(s['var'])
+        s['sec'] = s.get('sec',t['sec'])
+        if s['sec'] <= 0:
+            logger.critical("Seconds should be positive.")
+            raise ValueError(s['sec'])
+        s['end'] = s.get('end',t['end'])
+        s['nframes'] = seconds(s['sec'])
+    nframes = sum([ s['nframes'] for s in steps ])
+
     with writer.saving(fig,args.output,dpi):
         with ProgressBar(max_value=nframes) as bar:
             bar.start()
             i = 0
             for step in steps:
-                vrange = slow_range(step['getter'](),step['to'],num=step['nframes'])
-                setter = step['setter']
+                var = step['var']
+                getter = getters[var]
+                setter = setters[var]
+                end = step['end']
+                num = step['nframes']
+                vrange = slow_range(getter(),end,num=num)
                 for x in vrange:
                     setter(x)
                     writer.grab_frame()
